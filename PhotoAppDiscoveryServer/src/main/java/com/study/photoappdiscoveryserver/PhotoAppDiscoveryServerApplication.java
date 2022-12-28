63fc12018ef7345027749da8f98bb7d6c781371b
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-28 22:57:29 +0530

    adding Eureka discovery server

---
+++ PhotoAppDiscoveryServer/src/main/java/com/study/photoappdiscoveryserver/PhotoAppDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.study.photoappdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServerApplication.class, args);
+    }
+
+}

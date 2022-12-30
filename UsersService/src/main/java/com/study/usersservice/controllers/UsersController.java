898ff74795b33be281be36675aea2d0af51008a0
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-30 14:28:49 +0530

    adding jackson-dataformat-xml and produces/consumes on POSTMapping to support accepting and sending responses in xml format along with json

--- UsersService/src/main/java/com/study/usersservice/controllers/UsersController.java
+++ UsersService/src/main/java/com/study/usersservice/controllers/UsersController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,8 @@ public class UsersController {
         return "working on port : " + env.getProperty("local.server.port");
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<User> createUser(@RequestBody @Valid CreateUserRequestDto userReq) {
         User user = usersService.createService(userReq.toUser());
         return new ResponseEntity<>(user, HttpStatus.CREATED);
